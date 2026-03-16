e3d7a0cb735f26ebbde6f55b26c0f02c3b5937c9
upkirat17 <170941964+Upkirat17@users.noreply.github.com>
2026-03-16 11:34:40 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.7 MB (48.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int temp;
+
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (nums[i] > nums[j]) {
-                    temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+                    swap(nums[i], nums[j]);
                 }
             }
         }
